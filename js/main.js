0131ae487ce883235defe80165b8d002463b1793
ermonindima <ermonindima0@gmail.com>
2024-04-25 10:40:42 +0300

    50.54

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@
  const linkColor = document.querySelectorAll('.nav_link');
 
  function colorLink(){
-  linkColor.forEach(l = l.classList.remove('active'));
+  linkColor.forEach(l => l.classList.remove('active'));
   this.classList.add('active');
  }
 
